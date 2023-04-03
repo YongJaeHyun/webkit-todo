56d4c2595d8810bc93bcc475c2261ff4701d2984
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-04-03 16:09:49 +0900

    11. 목록 추가, 수정, 삭제까지 클래스로 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Container, List, Paper } from "@material-ui/core";
+import Todo from "./Todo";
+import AddTodo from "./AddTodo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { id: "todo0", title: "Todo 1", done: true },
+        { id: "todo1", title: "Todo 2", done: false },
+      ],
+    };
+  }
+
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length;
+    item.done = false;
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items: ", this.state.items);
+  };
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      console.log("Update Items: ", this.state.items);
+    });
+  };
+  render() {
+    const todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
+        <List>
+          {this.state.items.map((item) => (
+            <Todo item={item} key={item.id} delete={this.delete} />
+          ))}
+        </List>
+      </Paper>
+    );
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,73 @@
+import {
+  Checkbox,
+  IconButton,
+  InputBase,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+} from "@material-ui/core";
+import { DeleteOutlined } from "@material-ui/icons";
+import React from "react";
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: props.item, readOnly: true };
+    this.delete = props.delete;
+  }
+
+  deleteEventHandler = () => {
+    this.delete(this.state.item);
+  };
+  offReadOnlyMode = () => {
+    console.log("Event!", this.state.readOnly);
+    this.setState({ readOnly: false }, () => {
+      console.log("ReadOnly?", this.state.readOnly);
+    });
+  };
+  enterKeyEventHandler = (e) => {
+    if (e.key === "Enter") {
+      this.setState({ readOnly: true });
+    }
+  };
+  editEventHandler = (e) => {
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({ item: thisItem });
+  };
+  checkboxEventHandler = () => {
+    console.log("check box event call");
+    const thisItem = this.state.item;
+    thisItem.done = !thisItem.done;
+    this.setState({ item: thisItem });
+  };
+  render() {
+    const item = this.state.item;
+    return (
+      <ListItem>
+        <Checkbox checked={item.done} onChange={this.checkboxEventHandler} />
+        <ListItemText>
+          <InputBase
+            inputProps={{ "aria-label": "naked", readOnly: this.state.readOnly }}
+            type="text"
+            id={item.id}
+            name={item.id}
+            value={item.title}
+            multiline={true}
+            fullWidth={true}
+            onClick={this.offReadOnlyMode}
+            onChange={this.editEventHandler}
+            onKeyPress={this.enterKeyEventHandler}
+          />
+        </ListItemText>
+        <ListItemSecondaryAction>
+          <IconButton aria-label="Delete" onClick={this.deleteEventHandler}>
+            <DeleteOutlined />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+  }
+}
+
+export default Todo;
