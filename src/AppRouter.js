6ac26bbfdf75ddf12d9913e33290c7ec8ea41d4a
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-04-07 11:20:27 +0900

    15. 클라이언트 사이드 렌더링, JWT Token, Spring Security를 이용한 로그인 로직 구현 (함수형)

---
+++ src/AppRouter.js
@@ -0,0 +1,35 @@
+import { Box, Typography } from "@material-ui/core";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Login from "./Login";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      fsoftwareengineer, {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<App />} />
+        </Routes>
+      </div>
+      <div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
